aad3b33b8fdbc5685110a59e5532952dd9aa1435
sunmin1990 <63627305+sunmin1990@users.noreply.github.com>
2020-05-28 15:47:27 +0900

    Update java11_10.java

--- sunmin/java11_10.java
+++ sunmin/java11_10.java
@@ -25,3 +25,32 @@ public class java11_10 {
 		}
 	}
 }
+
+
+또는 
+
+package CollectionFrameWork;
+import java.util.*;
+
+public class java11_10 {
+	public static void main(String[] args) {
+		Set set = new LinkedHashSet();
+		int[][] board = new int[5][5];
+		
+		for(int i=0; set.size() <25; i++) {
+			set.add((int)(Math.random()*30)+1+"");
+		}
+		
+		
+		Iterator itr = set.iterator();
+		
+		for(int i=0; i<board.length; i++) {
+			for(int j =0; j<board[i].length; j++) {
+				board[i][j] = Integer.parseInt((String)itr.next());
+				System.out.print((board[i][j]<10? " " : " ")
+												+board[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
