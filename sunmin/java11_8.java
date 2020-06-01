0723df6eb8a2d1b7487a22c7daac900854945917
sunmin1990 <63627305+sunmin1990@users.noreply.github.com>
2020-06-01 17:51:10 +0900

    final java 11_8

--- sunmin/java11_8.java
+++ sunmin/java11_8.java
@@ -23,14 +23,15 @@ class Student8 implements Comparable{
 	    
 	    total = kor+eng+math;
 	}
-	int getTotal() { return total;}
+	int getTotal() { return total;} 
 	
 	float getAverage() {return (int)((getTotal()/3f)*10+0.5) / 10f;}
-	
+	//사실 여기가 해석이 잘 안된다. 
 	public int compareTo(Object o) {
 		if(o instanceof Student8) {
-			Student8 tmp = (Student8)o;
-			return tmp.total - this.total; 
+			Student8 s = (Student8)o;
+			return s.total - this.total; //왜 빼줘야 할까?
+			//return this.total < s.total ? 1 : -1;
 		} else {
 			return -1;
 		}
@@ -40,25 +41,28 @@ class Student8 implements Comparable{
 	kor+","+eng+","+math+","+
 				getTotal()+","+getAverage()+","+schoolRank;
 	}
-}//class Student
+}
 
 public class java11_8 {
 	
 	public static void calculateSchoolRank(List list) {
-		Collections.sort(list); 
-		int prevRank = -1;
-		int prevTotal = -1;
+		Collections.sort(list); //먼저 list 총점기준 내림차순으로정렬한다.
+		int prevRank = -1; //이전의 등수 -1을 해준 이유는? 순서를 다르게 하기 위해?
+		int prevTotal = -1; //이전의 총점
 		int length = list.size();
 		
-		
-
+		//for문으로 list 읽기 
 		for (int i=0; i<length;i++) {
 			Student8 s = (Student8)list.get(i);
+			//total이 preTotal과 같으면  prevRank쓰기
 			if (s.total == prevTotal) {
 				s.schoolRank = prevRank;
-			} else {
+			} 
+			//total이 preTotal과 다르면  새로 저장, 동점자 수 고려하기.
+			else {
 				s.schoolRank = i + 1;
 			}
+			//등수와 총점을 저장. 
 			prevRank = s.schoolRank;
 			prevTotal = s.total;
 		}
@@ -71,12 +75,12 @@ public class java11_8 {
 		list.add(new Student8("홍길동", 1, 3, 100, 100, 100));
 		list.add(new Student8("남궁성", 1, 1, 90, 70, 80));
 		list.add(new Student8("김자바", 1, 2, 80, 80, 90));
-		
+		// calculateSchoolRank에 list를 넣어서 실행~
 	    calculateSchoolRank(list);
-	    
-	    Iterator it = list.iterator();
-	    
-	    while(it.hasNext())
-	    	System.out.println(it.next());
+	    // list의 반복자를 itr에 넣는다.
+	    Iterator itr = list.iterator();
+	    //다음에 올게 있으면 ? 다음것을 출력한다.~
+	    while(itr.hasNext())
+	    	System.out.println(itr.next());
 	}
 }
